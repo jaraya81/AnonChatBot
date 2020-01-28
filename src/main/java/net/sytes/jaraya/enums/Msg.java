c6c7827d89fc19bfc00083c6fd026af4f584eca3
Julián Araya <julian.e.araya81@gmail.com>
2020-01-27 23:07:20 -0300

    add lang files

--- src/main/java/net/sytes/jaraya/enums/Msg.java
+++ src/main/java/net/sytes/jaraya/enums/Msg.java
@@ -1,7 +1,7 @@
 package net.sytes.jaraya.enums;
 
 public enum Msg {
-    START_OK("WELCOME_START"),
+    START_OK("START_OK"),
     START_BANNED_USER("START_BANNED_USER"),
     START_AGAIN("START_AGAIN"),
     USER_PLAY("USER_PLAY"),
@@ -11,28 +11,17 @@ public enum Msg {
     USER_2_NEXT_OK("USER_2_NEXT_OK"),
     USER_NEXT_WAITING("USER_NEXT_WAITING"),
     USER_NO_CHAT("USER_NO_CHAT"),
-
     SET_BIO_OK("SET_BIO_OK"),
-
     BIO("BIO"),
-
     CONFIG("CONFIG"),
     CHAT_TIMEOUT("CHAT_TIMEOUT"),
-
     USER_BLOCK("USER_BLOCK"),
-
     ABOUT("ABOUT"),
-
     NEXT_YOU("NEXT_YOU"),
-
     INACTIVITY_USER("INACTIVITY_USER"),
-
     SET_LANG_OK("SET_LANG_OK"),
-
     LANG("LANG"),
-
     USER_ACTIVE("USER_ACTIVE"),
-
     NEW_BIO("NEW_BIO");
 
     private String code;
