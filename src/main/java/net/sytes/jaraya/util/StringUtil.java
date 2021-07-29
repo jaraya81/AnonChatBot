81aff95eae0f4d880a5bf2647d3d8ba7971ea2de
julián <jaraya@microsystem.cl>
2021-07-28 20:58:56 -0400

    constructores

--- src/main/java/net/sytes/jaraya/util/StringUtil.java
+++ src/main/java/net/sytes/jaraya/util/StringUtil.java
@@ -1,6 +1,10 @@
 package net.sytes.jaraya.util;
 
 public class StringUtil {
+
+    private StringUtil() {
+    }
+
     public static String clean(String text) {
         if (text == null) return "";
         return text.replace(")", "\\)")
--- src/main/java/net/sytes/jaraya/util/Validation.java
+++ src/main/java/net/sytes/jaraya/util/Validation.java
@@ -3,6 +3,9 @@ package net.sytes.jaraya.util;
 import net.sytes.jaraya.exception.CoreException;
 
 public class Validation {
+    private Validation() {
+    }
+
     public static void check(boolean condition, String msg) throws CoreException {
         if (!condition) {
             throw new CoreException(msg);
